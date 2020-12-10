f5dcaceffddda86f3840615be684148904f6d11b
CalSchilling <CalSchilling@users.noreply.github.com>
2020-12-10 10:11:04 -0500

    Create Dashcam.py

---
+++ Dashcam.py
@@ -0,0 +1,7 @@
+#This is a comment
+#Going to be relearning Python as I create the program so if it looks like a mess of notes they are to remind me of what I am doing 
+"""
+Multi Line Comment
+"""
+
+import picamera
